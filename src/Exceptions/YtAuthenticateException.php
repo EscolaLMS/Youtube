21cd9e2acf22ce282cb98f39dd38ca01b9e17986
Hubert Krzysztofiak <hubert.krzysztofiak@escolasoft.com>
2022-04-28 09:00:43 +0200

    Add service exceptions

---
+++ src/Exceptions/YtAuthenticateException.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace EscolaLms\Youtube\Exceptions;
+
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Throwable;
+
+class YtAuthenticateException extends Exception
+{
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        $message = $message ?: __('Youtube stream if not exists or is not authorize, log in.');
+        $code = $code ?: 400;
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function render($request): JsonResponse
+    {
+        return response()->json([
+            'data' => [
+                'code' => $this->getCode(),
+                'message' => $this->getMessage()
+            ]
+        ], $this->getCode());
+    }
+}
+
+
